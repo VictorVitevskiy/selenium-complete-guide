3b31a027e9ab6a13495760b9c42890d825b2cc26
Victor Vitevskiy <vitevskiy@mail.ru>
2022-06-14 07:44:09 +0300

    Refactor CheckStickerTest.java

--- src/test/java/ru/vitevskiy/guide/selenium/CheckStickerTest.java
+++ src/test/java/ru/vitevskiy/guide/selenium/CheckStickerTest.java
@@ -1,27 +1,24 @@
 package ru.vitevskiy.guide.selenium;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class CheckStickerTest extends BaseTest {
 
     @Test
-    void checkStickerPresentTest() throws Exception {
+    void checkStickerPresentTest() {
         driver.get("http://localhost/litecart/");
 
-        List<WebElement> products = driver.findElements(By.xpath("//*[@class='image-wrapper']"));
+        List<WebElement> products = driver.findElements(By.cssSelector("li.product"));
 
         for (WebElement product : products) {
-            List<WebElement> sticker = product.findElements(By.xpath("./*[contains(@class, 'sticker')]"));
-            if (sticker == null || sticker.size() > 1) {
-                throw new Exception("Wrong number of stickers");
-            }
-            String stickerText = sticker.get(0).getText();
-            Assertions.assertTrue(stickerText.equals("NEW") || stickerText.equals("SALE"));
+            List<WebElement> sticker = product.findElements(By.cssSelector(".sticker"));
+            assertNotNull(sticker);
         }
     }
 }
