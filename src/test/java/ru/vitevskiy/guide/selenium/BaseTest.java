b5b9dc6bae2403941700bf2ae053cdd48603ca6b
Victor Vitevskiy <vitevskiy@mail.ru>
2022-06-14 09:06:18 +0300

    Refactor some files to run multiply tests

--- src/test/java/ru/vitevskiy/guide/selenium/BaseTest.java
+++ src/test/java/ru/vitevskiy/guide/selenium/BaseTest.java
@@ -2,30 +2,19 @@ package ru.vitevskiy.guide.selenium;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-
-import java.util.List;
 
 public class BaseTest {
 
-    public static WebDriver driver = Browser.getDriver();
+    protected WebDriver driver;
 
-//    @BeforeEach
-//    public void start(){
-//        driver.get("http://localhost/litecart/admin/");
-//    }
-
-    @AfterEach
-    public void stop() {
-        if (driver != null) {
-            driver.quit();
-        }
-        driver = null;
+    @BeforeEach
+    void start() {
+        driver = Browser.getInstance().getDriver();
     }
 
-    public boolean isElementPresent(By locator) {
-        return driver.findElements(locator) != null;
+    @AfterEach
+    void stop() {
+        Browser.getInstance().driverQuit();
     }
 }
